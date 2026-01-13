564762951902b29ce01674f33f3ab072a9d5a311
jnroot0101 <r_jnah@etu.enset-media.ac.ma>
2026-01-13 20:22:38 +0100

    implement add right and left

--- main.py
+++ main.py
@@ -2,20 +2,33 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
+
+        delta_time = clock.tick(60) / 1000
+        dt += delta_time
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 
